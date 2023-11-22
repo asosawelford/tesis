b9bb16c4fe4e2ffda957c1474eec9879acbd834e
asosawelford <alejandrososawelford@gmail.com>
2023-11-22 19:21:22 -0300

    server config and results 22-11

--- frontend/public/assets/js/config.js
+++ frontend/public/assets/js/config.js
@@ -1,6 +1,6 @@
-// const url = "http://localhost:8000";
+const url = "http://localhost:8000";
 // const url = "http://35.247.199.243:8000";
-const url = "http://34.95.169.200:8000";
+// const url = "http://34.95.169.200:8000";
 
 // const url = "http://192.168.1.36:8000"; // Ale
 
