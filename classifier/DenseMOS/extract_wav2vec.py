d3e18895f970ca0ba1236376d1c501f08c352b8a
asosawelford <alejandrososawelford@gmail.com>
2024-05-10 00:19:36 -0300

    finish up wav2vec extraction and fix some things about the test-dev-train splits

--- classifier/DenseMOS/extract_wav2vec.py
+++ classifier/DenseMOS/extract_wav2vec.py
@@ -1,37 +1,65 @@
 # wav2vec
+import os
+import numpy as np
 import pandas as pd
 import torch
 from tqdm.auto import tqdm
-from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model, Wav2Vec2Config
 import librosa
 
-
-# define function for extracting embeddings
-def wav2vec_embeddings(path_to_csv, model_name):
+def wav2vec_embeddings(path_to_csv, output_dir, model_name='facebook/wav2vec2-base-960h'):
     """Extracts embeddings from audio files given a specific Wav2Vec2 model.
-    Last hidden state is averaged to get a single embedding per audio file.
+    Extracts all layers, stacks and saves them as a numpy array.
     Expects csv file with a column named "stimuli" containing paths to audio files."""
 
+    os.makedirs(output_dir, exist_ok=True)
+
+    config = Wav2Vec2Config.from_pretrained(model_name, output_hidden_states=True)
     feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
-    model = Wav2Vec2Model.from_pretrained(model_name)
+    model = Wav2Vec2Model(config)
+
+    #load data
     metadata_df = pd.read_csv(path_to_csv)
-    # Create a new column to store embeddings
-    metadata_df['embeddings'] = None
+
     # iterate over each path in "stimuli" column
-    for index, row in tqdm(metadata_df.iterrows()):
+    for _, row in tqdm(metadata_df.iterrows()):
         audio_file = row['stimuli']
         input_audio, sample_rate = librosa.load(audio_file, sr=16000)
         inputs = feature_extractor(input_audio, return_tensors="pt", sampling_rate=sample_rate)
+
         with torch.no_grad():
-            outputs= model(inputs.input_values)
-        embeddings = outputs.last_hidden_state
+            outputs= model(**inputs)
+
+        all_layer_embeddings = outputs.hidden_states
+
+        # Concatenate hidden states from all layers and take the mean across the sequence
+        # first layer --> output of the CNN layers with an added positional embedding
+        embeddings = torch.cat(all_layer_embeddings, dim=0)
         embeddings = embeddings.mean(dim=1)
+
+        # Save embeddings as numpy array
         embeddings = embeddings.squeeze().detach().numpy()
-        metadata_df.at[index, 'embeddings'] = [embeddings]
+        file_name = os.path.basename(audio_file).split('.')[0]
+        file_folder = os.path.basename(os.path.dirname(audio_file).split('/')[-1])
+        os.makedirs(os.path.join(output_dir, f'{file_folder}'), exist_ok=True)
 
-    return metadata_df
+        np.save(os.path.join(output_dir, f'{file_folder}', f'{file_name}.npy'), embeddings)
 
-def save_embeddings(embeddings_df, path_to_save):
-    """Saves the embeddings to a csv file."""
-    embeddings_df.to_csv(path_to_save, index=False)
-    print(f"Embeddings saved at {path_to_save}")
+        # free up memory
+        del embeddings
+
+if __name__ == '__main__':
+    # extract embeddings for dev set
+    path_to_dev_csv = '/home/aleph/tesis/classifier/dev.csv'
+    path_to_dev_embeddings = '/home/aleph/tesis/classifier/embeddings/dev'
+    wav2vec_embeddings(path_to_dev_csv, path_to_dev_embeddings)
+
+    # extract embeddings for test set
+    path_to_test_csv = '/home/aleph/tesis/classifier/test.csv'
+    path_to_test_embeddings = '/home/aleph/tesis/classifier/embeddings/test'
+    wav2vec_embeddings(path_to_test_csv, path_to_test_embeddings)
+
+    # extract embeddings for train set
+    path_to_train_csv = '/home/aleph/tesis/classifier/train_merged.csv'
+    path_to_train_embeddings = '/home/aleph/tesis/classifier/embeddings/train'
+    wav2vec_embeddings(path_to_train_csv, path_to_train_embeddings)
